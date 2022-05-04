6655ae57fbd72d9cdc9a6fe8ca9e56156471c99f
anibal diaz <anibal_diaz@grupoasica.com>
2022-05-04 14:57:27 -0600

    añadiendo ejs

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+const port = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/src/views')
+
+app.use(express.static(__dirname + "/src"))
+
 app.get('/', function(req, res) {
-  res.send('Hola Mundo!');
+  res.render('index',{titulo: "Si funciona!"});
 });
 
-app.listen(8080, function() {
-  console.log('Aplicación ejemplo, escuchando el puerto 8080!');
+app.listen(port, function() {
+  console.log(`Aplicación ejemplo, escuchando el puerto ${port}!`);
 });
