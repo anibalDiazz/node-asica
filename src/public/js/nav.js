a0a9de9f8c5c6dae561aea561cb15bea78534db3
anibal diaz <anibal_diaz@grupoasica.com>
2022-05-05 17:11:11 -0600

    mejorando nav

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -2,5 +2,20 @@ console.log("sirve");
 const d = document;
 
 d.getElementById('menuHam').onclick = function(){
-    console.log("sirve")
-    d.getElementById('vertical').classList.toggle('active')}
+    d.getElementById('vertical').classList.toggle('active')
+}
+
+d.getElementById("blogMenu").onclick = function(){
+    console.log('entra')
+    d.getElementById('blogSubmenu').classList.toggle('activeSubmenu')
+}
+
+d.getElementById("productoMenu").onclick = function(){
+    console.log('entra')
+    d.getElementById('productosSubmenu').classList.toggle('activeSubmenu')
+}
+
+d.getElementById("contactoMenu").onclick = function(){
+    console.log('entra')
+    d.getElementById('contactoSubmenu').classList.toggle('activeSubmenu')
+}
